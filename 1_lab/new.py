c0eb1e525bcd2f05228213898344ae513a573879
BobasB <bugil.bogdan@gmail.com>
2024-01-31 11:53:51 +0200

    start creating virtualenv

---
+++ 1_lab/new.py
@@ -0,0 +1,6 @@
+import numpy as np
+print(f"Ура програма запустилась! Ми користоємось бібліотекою numpy {np.__version__}", np.arange(10))
+
+#import requests
+#r = requests.get('https://api.github.com/events')
+#print(f"Відповідь {r.status_code}")
