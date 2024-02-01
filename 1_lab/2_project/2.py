1b373676e79144ae09728112cb2f4277315f0320
BobasB <bugil.bogdan@gmail.com>
2024-02-01 13:02:13 +0200

    work with multiple venvs

---
+++ 1_lab/2_project/2.py
@@ -0,0 +1,2 @@
+import numpy as np
+print(f"Ура програма запустилась! Ми користоємось бібліотекою {np.__name__} {np.__version__}", np.arange(10))
--- 1_lab/new.py
+++ 1_lab/new.py
@@ -1,6 +1,1 @@
-import numpy as np
-print(f"Ура програма запустилась! Ми користоємось бібліотекою numpy {np.__version__}", np.arange(10))
-
-#import requests
-#r = requests.get('https://api.github.com/events')
-#print(f"Відповідь {r.status_code}")
+print(f"Ура програма запустилась! Ця програма не потребує використання сторонніх бібліотек!")
