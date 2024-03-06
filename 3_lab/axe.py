189926d18e1d939e50533ddf53a939b0ae2f6e21
BobasB <bugil.bogdan@gmail.com>
2024-03-06 11:33:08 +0200

    unittest

---
+++ 3_lab/axe.py
@@ -0,0 +1,8 @@
+class Axe:
+    def __init__(self) -> None:
+        self.name = "Сокира"
+        self.damag = 0
+        self.vitality = 0
+
+    def __repr__(self) -> str:
+        return "Swords()"
